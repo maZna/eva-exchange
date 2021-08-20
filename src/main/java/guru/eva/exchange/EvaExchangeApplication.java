930dca2845c11b997794ffb8eb2fa034c2fe0756
maZna <masnaahmed1998@gmail.com>
2021-08-20 19:46:59 +0300

    Initial commit

---
+++ src/main/java/guru/eva/exchange/EvaExchangeApplication.java
@@ -0,0 +1,13 @@
+package guru.eva.exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/eva/exchange/EvaExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.eva.exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
